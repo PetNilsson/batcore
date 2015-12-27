d88f10745ab1aa2659f0c0ac37155f398537688d
Peter Nilsson <peter@pelotek.se>
2015-12-27 10:42:09 +0100

    Added database connection

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
-from app import app
+from app import app, db
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
+@app.route('/dbtest')
+def dbtest():
+    if db.session.query("1").from_statement("SELECT 1").all():
+        return 'It works.'
+    else:
+        return "It doesn't work"
